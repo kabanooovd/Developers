1a5c41b33877bc94c50ab45397b78ff0b04eebbb
dimas <kabanooovd@yandex.ru>
2022-10-01 17:40:42 +0300

    DEVS-2

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,31 @@
+import { config } from "dotenv"
+config()
 import express, { Request, Response } from "express";
+import sequelize from './db/db'
+import cors from "cors"
+
+// console.log("==> 777 ", process.env.PORT)
 
 const app = express();
+const PORT = process.env.PORT || 5000
 
-const PORT = 5000
+app.use(cors())
+app.use(express.json())
 
 app.use("/", (req, res) => {
   res.send("HELLO WORLD")
 })
 
-const start = () => {
-  app.listen(PORT, () => {
-    console.log(`app has startd on ${PORT} port`);
-  });
+const start = async () => {
+  try {
+    await sequelize.authenticate()
+    await sequelize.sync()
+    app.listen(PORT, () => {
+      console.log(`app has startd on ${PORT} port`);
+    });
+  } catch(error) {
+    console.log('==> error ', error)
+  }
 };
 
 start()
